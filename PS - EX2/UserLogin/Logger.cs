0719ee482c98c5af33099610c465581d695c7787
yo23ana <102253726+yo23ana@users.noreply.github.com>
2022-05-13 18:05:29 +0300

    PS - EX2 - upload

---
+++ PS - EX2/UserLogin/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLogin
+{
+        static class Logger
+        {
+            private static List<string> currentSessionActiviies = new List<string>();
+
+            public static void LogActivity(string activity)
+            {
+                string activityLine = DateTime.Now + ";" + LoginValidation.CurrentUsername + ";" +
+                                      LoginValidation.CurrentUserRole + ";" + activity;
+
+                if (File.Exists("test.txt") == true)
+                {
+                    File.AppendAllText("test.txt", activityLine);
+                }
+
+                currentSessionActiviies.Add(activityLine);
+            }
+
+            public static string GetCurrentSessionActivities()
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var curr in currentSessionActiviies)
+                {
+                    sb.Append(curr);
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+
